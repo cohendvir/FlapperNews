0fd3d875b02eaaea69fcf17270f6fca64fe63552
Dvir Cohen <dvircohen100@gmail.com>
2016-01-02 18:12:17 +0200

    adding controllers logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+
+  before_action :find_post, only: [:create, :upvote]
+
+  def create
+    comment = @post.comments.create(comment_params)
+    respond_with @post, comment
+  end
+
+  def upvote
+    comment = @post.comment.find(params[:id])
+    comment.increment!(:upvotes)
+    respond_with @post, comment
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:show, :upvote]
+
+  def index
+    respond_with Post.all
+  end
+
+  def create
+    respond_with Post.create(post_params)
+  end
+
+  def show
+    respond_with @post
+  end
+
+  def upvote
+    @post.increment!(:upvotes)
+    respond_with @post
+  end
+
+  private
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:link, :title)
+  end
 end

31df50804087403b1068b45adc9cef9296385d5c
Dvir Cohen <dvircohen100@gmail.com>
2016-01-03 12:00:05 +0200

    default upvotes to 0 in database

---
+++ db/migrate/20160103095122_set_default_upvotes.rb
@@ -0,0 +1,5 @@
+class SetDefaultUpvotes < ActiveRecord::Migration
+  def change
+    change_column_default :posts, :upvotes, 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102151001) do
+ActiveRecord::Schema.define(version: 20160103095122) do
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160102151001) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "link"
-    t.integer  "upvotes"
+    t.integer  "upvotes",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end

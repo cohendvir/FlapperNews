9cb90ee78b7627187617861b496a2a0abef076ab
Dvir Cohen <dvircohen100@gmail.com>
2016-01-07 13:02:14 +0200

    change service to work with mongoid id's

--- app/assets/javascripts/posts/comments_service.js
+++ app/assets/javascripts/posts/comments_service.js
@@ -5,21 +5,23 @@ app.factory('comments', function($http) {
     var obj = {
 
         upvoteComment: function(post, comment){
-            return $http.put('/posts/' + post.id + '/comments/' + comment.id + '/upvote').success(function(data) {
+            return $http.put('/posts/' + post.id + '/comments/' + comment._id + '/upvote').success(function(data) {
                 comment.upvotes = data.upvotes;
             });
         },
 
         addComment: function(post, comment) {
             return $http.post('/posts/' + post.id + '/comments', comment).success(function(data) {
+                data._id = data.id;
+                delete data.id;
                 post.comments.push(data);
             });
         },
 
         removeComment: function(post, comment) {
-            return $http.delete('/posts/' + post.id + '/comments/' + comment.id).success(function() {
+            return $http.delete('/posts/' + post.id + '/comments/' + comment._id).success(function() {
                 var index = _.findIndex(post.comments, function(c) {
-                    return c.id == comment.id;
+                    return c._id == comment._id;
                 });
                 post.comments.splice(index, 1);
             });
